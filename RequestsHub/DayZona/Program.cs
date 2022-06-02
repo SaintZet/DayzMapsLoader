df7788d1667503088d1a180eaff85be1ea510082
Serhii Chepets <48261970+SaintZet@users.noreply.github.com>
2022-06-02 09:37:34 +0300

    Delete RequestsHub/DayZona directory
    Delete this directory because download same files with another names.

--- RequestsHub/DayZona/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using DayZona;
-using Maps;
-
-Livonia livonia = new Livonia();
-//livonia.GetAllImages(TypeOfMap.Terrain, Direction.Horizontal, 7, @"D:/LivoniaMap");
-MergePictures images = new MergePictures();
-images.GetPicturesFromFolder();
-
-//MergeImages.HorizontalMerge(@"E:\LivoniaMap\Horizontal 0");
