595e9a31f018e537287bdc845354b1b956ccdc09
Ярослав Тимченко <ytimchenko57@gmail.com>
2022-06-04 15:11:04 +0300

    Remove method

--- RequestsHub/RequestsHub/MergePictures.cs
+++ RequestsHub/RequestsHub/MergePictures.cs
@@ -61,32 +61,7 @@ namespace RequestsHub
             DirectoryInfo directory = new DirectoryInfo(currentDirectoryName);
 
             return directory.GetFiles().Select(s => s.FullName).OrderBy(s => s.Length).ThenBy(s => s);
-        }
-
-        private Bitmap ResizeImage(Image image, int width, int height)
-        {
-            Rectangle destRect = new Rectangle(0, 0, width, height);
-            Bitmap destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
-        }
+        }      
         
     }
 }
