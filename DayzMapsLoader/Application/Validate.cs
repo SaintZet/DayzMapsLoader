a1a4ecbe0b834ee8a39c057db723a50a2208f64f
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2022-10-31 20:02:37 +0200

    delete old validation

--- DayzMapsLoader/Application/Validate.cs
+++ DayzMapsLoader/Application/Validate.cs
@@ -1,50 +1,9 @@
-﻿using RequestsHub.Domain;
-using RequestsHub.Domain.Contracts;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace RequestsHub.Application;
 
 internal static class Validate
 {
-    public static bool CheckTypeAtMap(IMap currentMap, MapType typeMap)
-    {
-        foreach (var type in currentMap.TypesMap)
-        {
-            if (type == typeMap)
-            {
-                return true;
-            }
-        }
-        //TODO:Write error message.
-        throw new ArgumentException("");
-    }
-
-    public static bool CheckMapAtProvider(IMapProvider mapProvider, MapName nameMap)
-    {
-        foreach (var map in mapProvider.Maps)
-        {
-            if (map.Name == nameMap)
-            {
-                return true;
-            }
-        }
-        //TODO:Write error message.
-        throw new ArgumentException("");
-    }
-
-    internal static bool CheckZoomAtMap(IMap currentMap, int zoom)
-    {
-        foreach (var item in currentMap.ZoomLevelRatioToSize)
-        {
-            if (item.Key == zoom)
-            {
-                return true;
-            }
-        }
-        //TODO:Write error message.
-        throw new ArgumentException("");
-    }
-
     //TODO: write comment.
     internal static string PathToSave(string pathsToSave)
     {
