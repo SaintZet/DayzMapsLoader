78e42c2482117793f0649c69b212a229550e555c
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2022-10-31 20:04:25 +0200

    reworked from List to HastSet, add vcalidation

--- DayzMapsLoader/Domain/Contracts/IMapProvider.cs
+++ DayzMapsLoader/Domain/Contracts/IMapProvider.cs
@@ -1,11 +1,23 @@
-﻿using RequestsHub.Domain.MapsProviders;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using RequestsHub.Domain.MapsProviders;
 
 namespace RequestsHub.Domain.Contracts;
 
-internal interface IMapProvider
+public interface IMapProvider
 {
-    abstract List<IMap> Maps { get; }
+    abstract ISet<IMap> Maps { get; }
     abstract MapProvider Name { get; }
 
     string ToString();
+}
+
+public class MapProviderValidation : AbstractValidator<IMapProvider>
+{
+    public MapProviderValidation(IMap map)
+    {
+        RuleFor(provider => provider.Maps)
+            .Must(maps => maps.Contains(map))
+            .WithMessage($"Map {map.Name} doesn't exists in MapProvider");
+    }
 }
