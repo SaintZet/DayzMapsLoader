0629ffd1f4a788f5fef1fb80008f6d1ce52df491
mamay12 <ytimchenko57@gmail.com>
2023-02-10 23:52:06 +0200

    add improved context

--- src/DayzMapsLoader.Presentation.ReactWithVite/src/modules/DownloadMap/context/providedMapsContext.tsx
+++ src/DayzMapsLoader.Presentation.ReactWithVite/src/modules/DownloadMap/context/providedMapsContext.tsx
@@ -1,8 +1,37 @@
-import {ProvidedMap} from "../../../helpers/types";
-import {ProvidedMapsService} from "../api/providedMaps.services";
-import React from "react";
+import {MapProvider, ProvidedMap} from "../../../helpers/types";
+import {MapsProviderService, ProvidedMapsService} from "../api/providedMaps.services";
+import React, {FC, PropsWithChildren, useContext, useMemo, useState} from "react";
 
-const initialProvidedMaps: ProvidedMap[] = [];
-export const providedMaps = await ProvidedMapsService.getAll().then(data => data.data);
+export const getProvidedMaps = await ProvidedMapsService.getAll().then(data => data.data);
+export const useContextStore = () => useContext(DownloadMapContext);
 
-export const ProvidedMapsContext = React.createContext<ProvidedMap[]>(initialProvidedMaps);
+export const DownloadMapContext = React.createContext<ContextMembers>({
+    providedMaps: getProvidedMaps,
+    selectedProvider: {},
+} as ContextMembers);
+
+export const DownloadMapProvider: FC<PropsWithChildren> = ({children}) => {
+
+    const providedMaps = getProvidedMaps;
+    const [selectedProvider, setSelectedProvider] = useState<MapProvider>();
+
+    const value: ContextMembers = useMemo(
+        () => ({
+            providedMaps,
+            selectedProvider,
+            setSelectedProvider
+        }),
+        [
+            providedMaps,
+            selectedProvider,
+            setSelectedProvider
+        ]
+    );
+
+    return <DownloadMapContext.Provider value={value}>{children}</DownloadMapContext.Provider>
+};
+
+interface ContextMembers {
+    providedMaps: ProvidedMap[],
+    selectedProvider: MapProvider | undefined,
+}
