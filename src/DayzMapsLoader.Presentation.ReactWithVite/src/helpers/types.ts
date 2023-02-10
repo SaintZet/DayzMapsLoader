bd7c1c046041281bf53d45c41e6085148af49944
mamay12 <ytimchenko57@gmail.com>
2023-02-10 14:27:16 +0200

    add file witj types

---
+++ src/DayzMapsLoader.Presentation.ReactWithVite/src/helpers/types.ts
@@ -0,0 +1,61 @@
+import {Provider} from "react";
+
+export interface MapProvider {
+    id: number;
+    name: string;
+    link: string;
+}
+
+export interface Map {
+    id: number;
+    name: string;
+    description: string;
+    lastVersion: string;
+    author: string;
+    link: string;
+}
+
+export interface ZoomLevelRatioSize {
+    height: number;
+    width: number;
+}
+
+export interface MapType {
+    id: number;
+    name: string;
+}
+
+export interface ProvidedMap {
+    id: number;
+    nameForProvider: string;
+    mapProvider: MapProvider;
+    map: Map;
+    mapType: MapType;
+    maxTypeForProvider: string;
+    maxMapLevel: number;
+    isFirstQuadrant: boolean;
+    version: string;
+    imageExtension: string;
+}
+
+export type LoaderSelectorTypes = Array<MapProvider> | Array<MapType> | Array<Map> | Array<number>;
+
+type ProviderWithChanges<T extends MapProvider> = {
+    [K in keyof T & string as `${K}Provider`]: T[K]
+};
+
+export type MappedProvider = ProviderWithChanges<MapProvider>;
+
+type MapTypeWithChanges<T extends MapType> = {
+    [K in keyof T & string as `${K}Type`]: T[K]
+};
+
+export type MappedMapType = MapTypeWithChanges<MapType>;
+
+export enum InterfaceMatcher {
+    "MapArray",
+    "MapTypeArray",
+    "ZoomArray",
+    "ProviderArray",
+    "Default" ,
+}
