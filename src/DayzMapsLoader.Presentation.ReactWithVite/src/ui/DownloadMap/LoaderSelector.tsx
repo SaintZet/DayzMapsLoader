ba72739d1c5bfd24cc1ff198d6534396966fdf0f
mamay12 <ytimchenko57@gmail.com>
2023-02-12 16:38:01 +0200

    make unified form with validation

--- src/DayzMapsLoader.Presentation.ReactWithVite/src/ui/DownloadMap/LoaderSelector.tsx
+++ src/DayzMapsLoader.Presentation.ReactWithVite/src/ui/DownloadMap/LoaderSelector.tsx
@@ -1,24 +1,57 @@
 import {FormControl, InputLabel, Select} from "@mui/material";
 import {
-    LoaderTypes,
+    LoaderTypes, SelectItem,
 } from "../../helpers/types";
-import React, {ReactNode} from "react";
+import React, {ReactNode, useMemo} from "react";
+import {FormikProps} from "formik";
+import FormLabel from "@mui/material/FormLabel";
+import Stack from "@mui/material/Stack";
+import MenuItem from "@mui/material/MenuItem";
+import CircularProgress from "@mui/material/CircularProgress";
+import FormHelperText from "@mui/material/FormHelperText";
+import {string} from "yup";
 
-interface LoaderSelectorProps<T> {
-    settings?: { m: number, width: number },
-    menuItems: ReactNode,
-    className: string,
+interface FormikSettings<T>{
+    required?: boolean,
+    disabled?: boolean,
+    showError?: boolean,
+    controlName: keyof T,
+    label: string,
+    options: SelectItem<number>[],
 }
 
-export default function LoaderSelector<T extends LoaderTypes>(props: LoaderSelectorProps<T>): JSX.Element {
-    return (
-        <div className={props.className}>
-            <FormControl sx={props.settings}>
-                <InputLabel id={props.className}>{props.className}</InputLabel>
-                <Select labelId={props.className} label={props.className}>
-                    {props.menuItems}
-                </Select>
-            </FormControl>
-        </div>
-    )
-}
+export function Selector<T>(props: FormikProps<T> & FormikSettings<T>) {
+    const controlId = `select-${String(props.controlName)}`;
+    const meta = useMemo(() => props.getFieldMeta(String(props.controlName)), [props]);
+    return <FormControl fullWidth required={!!props.required} disabled={!!props.disabled} error={props.showError}>
+        <FormLabel error={Boolean(meta.error && meta.touched)}>{props.label}</FormLabel>
+        <Stack direction="row" width="100%" spacing={1}>
+            <Stack direction="column" justifyContent="center" alignItems="center" width='100%'>
+                {props.options?.length ? (
+                    <Select
+                        required={!!props.required}
+                        disabled={!!props.disabled}
+                        id={controlId}
+                        name={String(props.controlName)}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values[props.controlName] || -1}
+                        error={Boolean(meta.error && meta.touched)}
+                    >
+                        <MenuItem value={-1} disabled={!!props.required}>
+                            None
+                        </MenuItem>
+                        {props.options?.map((x, i) => (
+                            <MenuItem key={i} value={x.value}>
+                                {x.text}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                ) : (
+                    <CircularProgress />
+                )}
+                {props.showError && <FormHelperText>{meta.error}</FormHelperText>}
+            </Stack>
+        </Stack>
+    </FormControl>
+};
