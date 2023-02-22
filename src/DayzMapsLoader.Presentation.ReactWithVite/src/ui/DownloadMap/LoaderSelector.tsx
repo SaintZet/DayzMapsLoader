887f3c7e41372726578aad92486b478d891a8b97
mamay12 <ytimchenko57@gmail.com>
2023-02-22 21:25:05 +0200

    Small changes in Selector

--- src/DayzMapsLoader.Presentation.ReactWithVite/src/ui/DownloadMap/LoaderSelector.tsx
+++ src/DayzMapsLoader.Presentation.ReactWithVite/src/ui/DownloadMap/LoaderSelector.tsx
@@ -1,39 +1,39 @@
-import {FormControl, InputLabel, Select} from "@mui/material";
+import {FormControl, InputLabel, Select, SelectChangeEvent} from "@mui/material";
 import {
-    LoaderTypes, SelectItem,
+     SelectItem,
 } from "../../helpers/types";
-import React, {ReactNode, useMemo} from "react";
+import React, {useMemo} from "react";
 import {FormikProps} from "formik";
 import FormLabel from "@mui/material/FormLabel";
 import Stack from "@mui/material/Stack";
 import MenuItem from "@mui/material/MenuItem";
 import CircularProgress from "@mui/material/CircularProgress";
 import FormHelperText from "@mui/material/FormHelperText";
-import {string} from "yup";
 
 interface FormikSettings<T>{
     required?: boolean,
     disabled?: boolean,
     showError?: boolean,
-    controlName: keyof T,
+    controlName: keyof T & string,
     label: string,
-    options: SelectItem<number>[],
+    options: Set<SelectItem<number>>,
+    onHandleChange?: (event: SelectChangeEvent<number>) => void,
 }
 
 export function Selector<T>(props: FormikProps<T> & FormikSettings<T>) {
     const controlId = `select-${String(props.controlName)}`;
     const meta = useMemo(() => props.getFieldMeta(String(props.controlName)), [props]);
     return <FormControl fullWidth required={!!props.required} disabled={!!props.disabled} error={props.showError}>
-        <FormLabel error={Boolean(meta.error && meta.touched)}>{props.label}</FormLabel>
         <Stack direction="row" width="100%" spacing={1}>
             <Stack direction="column" justifyContent="center" alignItems="center" width='100%'>
-                {props.options?.length ? (
+                <FormLabel error={Boolean(meta.error && meta.touched)}>{props.label}</FormLabel>
+                {
                     <Select
                         required={!!props.required}
                         disabled={!!props.disabled}
                         id={controlId}
                         name={String(props.controlName)}
-                        onChange={props.handleChange}
+                        onChange={props.onHandleChange ?? props.handleChange}
                         onBlur={props.handleBlur}
                         value={props.values[props.controlName] || -1}
                         error={Boolean(meta.error && meta.touched)}
@@ -41,15 +41,13 @@ export function Selector<T>(props: FormikProps<T> & FormikSettings<T>) {
                         <MenuItem value={-1} disabled={!!props.required}>
                             None
                         </MenuItem>
-                        {props.options?.map((x, i) => (
+                        {[...props.options]?.map((x, i) => (
                             <MenuItem key={i} value={x.value}>
                                 {x.text}
                             </MenuItem>
                         ))}
                     </Select>
-                ) : (
-                    <CircularProgress />
-                )}
+                }
                 {props.showError && <FormHelperText>{meta.error}</FormHelperText>}
             </Stack>
         </Stack>
