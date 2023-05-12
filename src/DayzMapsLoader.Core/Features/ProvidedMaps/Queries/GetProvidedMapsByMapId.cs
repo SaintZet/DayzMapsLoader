887710022ae4abdb9567cb42690076806452f040
Serhii Chepets <chepets9685@gmail.com>
2023-05-13 01:03:38 +0300

    Bug fix. Wrong query.

--- src/DayzMapsLoader.Core/Features/ProvidedMaps/Queries/GetProvidedMapsByMapId.cs
+++ src/DayzMapsLoader.Core/Features/ProvidedMaps/Queries/GetProvidedMapsByMapId.cs
@@ -5,17 +5,17 @@ using MediatR;
 
 namespace DayzMapsLoader.Core.Features.ProvidedMaps.Queries;
 
-public record GetProvidedMapsByMapIdQuery(int ProviderId) : IRequest<IEnumerable<ProvidedMap>>;
+public record GetProvidedMapsByMapIdQuery(int MapId) : IRequest<IEnumerable<ProvidedMap>>;
 
-internal class GetProvidedMapsByMapIdHandler : IRequestHandler<GetProvidedMapsByProviderIdQuery, IEnumerable<ProvidedMap>>
+internal class GetProvidedMapsByMapIdHandler : IRequestHandler<GetProvidedMapsByMapIdQuery, IEnumerable<ProvidedMap>>
 {
     private readonly IProvidedMapsRepository _providedMapsRepository;
 
     public GetProvidedMapsByMapIdHandler(IProvidedMapsRepository providedMapsRepository)
         => _providedMapsRepository = providedMapsRepository;
 
-    public async Task<IEnumerable<ProvidedMap>> Handle(GetProvidedMapsByProviderIdQuery request, CancellationToken cancellationToken)
+    public async Task<IEnumerable<ProvidedMap>> Handle(GetProvidedMapsByMapIdQuery request, CancellationToken cancellationToken)
         => await _providedMapsRepository
-            .GetAllProvidedMapsByMapIdAsync(request.ProviderId)
+            .GetAllProvidedMapsByMapIdAsync(request.MapId)
             .ConfigureAwait(false);
 }
