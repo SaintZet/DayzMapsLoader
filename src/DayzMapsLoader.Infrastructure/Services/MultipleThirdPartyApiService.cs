e5e2f57fdadb336436ab7c705b4c91f35f048b93
mrlldd <roflanjaraxxus@gmail.com>
2023-07-24 20:19:11 +0300

    use parallel loading of image parts instead of sequential

--- src/DayzMapsLoader.Infrastructure/Services/MultipleThirdPartyApiService.cs
+++ src/DayzMapsLoader.Infrastructure/Services/MultipleThirdPartyApiService.cs
@@ -2,7 +2,7 @@
 using DayzMapsLoader.Core.Models;
 
 using DayzMapsLoader.Domain.Entities;
-
+using Nito.AsyncEx;
 using SmartFormat;
 
 namespace DayzMapsLoader.Infrastructure.Services;
@@ -18,7 +18,9 @@ public class MultipleThirdPartyApiService : IMultipleThirdPartyApiService
 
         var queryTemplate = map.MapProvider.UrlQueryTemplate;
 
+        var asyncLock = new AsyncLock();
         var yReversed = mapSize.Width - 1;
+        var loaders = new List<Func<Task>>(mapSize.Width * mapSize.Height);
         for (var y = 0; y < mapSize.Width; y++)
         {
             for (var x = 0; x < mapSize.Height; x++)
@@ -31,15 +33,28 @@ public class MultipleThirdPartyApiService : IMultipleThirdPartyApiService
                     Y = map.IsFirstQuadrant ? yReversed : y
                 };
 
-                var query = Smart.Format(queryTemplate, parameters);
+                var queryY = y;
+                var queryX = x;
+                loaders.Add(async () =>
+				{
+					var query = Smart.Format(queryTemplate, parameters);
 
-                var response = await _httpClient.GetAsync(query);
-                var data = await response.Content.ReadAsByteArrayAsync();
-
-                mapParts.AddPart(x, y, new MapPart(data));
+					var response = await _httpClient.GetAsync(query);
+					var data = await response.Content.ReadAsByteArrayAsync();
+					using var locked = await asyncLock.LockAsync();
+					mapParts.AddPart(queryX, queryY, new MapPart(data));
+				});
             }
 
             yReversed--;
+        }
+
+        var concurrentLimit = zoom * zoom;
+        while (loaders.Count != 0)
+        {
+	        var parallelRequests = loaders.Take(concurrentLimit).ToArray();
+	        await Task.WhenAll(parallelRequests.Select(x => x.Invoke()));
+	        loaders.RemoveRange(0, parallelRequests.Length);
         }
 
         return mapParts;
